d4378a8c5734da385d4117ef311f698a1d938545
Leandro Segovia <ldlsegovia@gmail.com>
2015-06-12 18:42:29 -0300

    feat(InstallGenerator): mount engine in host app

--- lib/generators/paperclip_upload/install_generator.rb
+++ lib/generators/paperclip_upload/install_generator.rb
@@ -1,6 +1,13 @@
 class PaperclipUpload::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
+  def mount_routes
+    line = "Rails.application.routes.draw do"
+    gsub_file "config/routes.rb", /(#{Regexp.escape(line)})/mi do |match|
+      "#{match}\n  mount PaperclipUpload::Engine => '/'\n"
+    end
+  end
+
   def copy_engine_migrations
     rake "railties:install:migrations"
   end
