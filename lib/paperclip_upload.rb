4617525742659da68d7ae2b65105b07bafc3191a
Leandro Segovia <ldlsegovia@gmail.com>
2015-06-19 15:35:46 -0300

    feat(): add additional_upload_endpoints method to configure alternative endpoints for /upload

--- lib/paperclip_upload.rb
+++ lib/paperclip_upload.rb
@@ -5,4 +5,20 @@ require "paperclip_upload/active_record_extension"
 require "paperclip_upload/engine"
 
 module PaperclipUpload
+  extend self
+
+  attr_accessor :additional_upload_endpoints
+
+  def draw_additional_upload_endpoints(_ctx)
+    return if !self.additional_upload_endpoints || additional_upload_endpoints.count.zero?
+    self.additional_upload_endpoints.each do |endpoint|
+      _ctx.get "#{endpoint}/:id" => "paperclip_upload/uploads#show"
+      _ctx.post "#{endpoint}" => "paperclip_upload/uploads#create"
+    end
+  end
+
+  def setup
+    yield self
+    require "paperclip_upload"
+  end
 end
