3614055da7fa5bb4f50d9d63cc02660aaba5feb1
Leandro Segovia <ldlsegovia@gmail.com>
2018-07-11 18:02:45 -0300

    feat(): remove verify_authenticity_token from uploads controller

--- app/controllers/rails_pallet/uploads_controller.rb
+++ app/controllers/rails_pallet/uploads_controller.rb
@@ -3,9 +3,6 @@ module RailsPallet
     self.responder = RailsPalletResponder
     respond_to :json
 
-    # protect_from_forgery with: :exception
-    # skip_before_action :verify_authenticity_token, raise: false
-
     def create
       new_upload = RailsPallet::Upload.create(permitted_params)
       respond_with new_upload, status: :created
